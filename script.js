aac07b4f84b6d4cf3b4dab2a5b0d4f540fea73ae
andicodetrf <andrea_lau92@hotmail.com>
2020-06-26 21:28:24 +0800

    added checkInputMatch and update current score when there's a match

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ console.log('>> script-ran <<')
 
 //global Var
 let isGameStart = false;
-const dictArray = ["position", " develop", " finish", " coerce", " sick", " preparation", " pin", " resource", " vote", " scheme", " theater", " blonde", " syndrome", " spectrum", " heaven", " present", " pluck", " ridge", " soldier", " liability", " extort", " cross", " equinox", " distributor", " promote", " fisherman", " misplace", " choose", " incredible", " costume", " amputate", " application", " conglomerate", " sanctuary", " dictate", " eaux", " grace", " myth", " architecture", " systematic", " expenditure", " trait", " earthwax", " union", " enemy", " justify", " skilled", " vain", " provision", " sunrise"]
-
+const dictArray = ["position", "develop", "finish", "coerce", "sick", "preparation", "pin", "resource", "vote", "scheme", "theater", "blonde", "syndrome", "spectrum", "heaven", "present", "pluck", "ridge", "soldier", "liability", "extort", "cross", "equinox", "distributor", "promote", "fisherman", "misplace", "choose", "incredible", "costume", "amputate", "application", "conglomerate", "sanctuary", "dictate", "eaux", "grace", "myth", "architecture", "systematic", "expenditure", "trait", "earthwax", "union", "enemy", "justify", "skilled", "vain", "provision", "sunrise"]
+let currentScore = 0;
+let totalScore = 0;
 
 //DOM selector
 let wordDisplay = document.querySelector('.word-display');
 let inputBox = document.querySelector('.input-box');
-
+let currentScoreDisplay = document.querySelector('.current-score')
+let buttonInit = document.querySelector('.btn-init')
 
 //Function
 
@@ -17,14 +19,17 @@ const genRandWord = () => {
     let rand = Math.floor(Math.random()*dictArray.length);
     let randWord = dictArray[rand];
 
-    console.log(randWord);
+    // console.log(randWord);
     wordDisplay.textContent = randWord;
 
 }
 
 const logInput = () =>{
     console.log(inputBox.value);
+    console.log(wordDisplay.textContent)
 }
+
+
 
 const clearInputBox = () =>{
     inputBox.value = "";
@@ -33,7 +38,16 @@ const clearInputBox = () =>{
 
 //FN-to check if word match
 
-
+const checkInputMatch = () =>{
+    console.log(inputBox.value);
+    console.log(wordDisplay.textContent);
+    if(inputBox.value === wordDisplay.textContent){
+        currentScore++
+    }
+    currentScoreDisplay.textContent = currentScore;
+    console.log(currentScore);
+    console.log(currentScoreDisplay);
+}
 
 
 
@@ -50,12 +64,26 @@ const clearInputBox = () =>{
 
 //Event Listener
 
-//e.keyCode 32 is for spacebar
+//e.keyCode 32 is for spacebar - will create space and mess with scoring
 //enter just put e.key === "Enter"
-inputBox.addEventListener('keypress', function(e){
-    if(e.keyCode == 32){
-        logInput();
+// inputBox.addEventListener('keypress', function(e){
+//     if(e.key == "Enter"){
+//         // logInput();
+//         checkInputMatch();
+//         clearInputBox();
+//     }
+// })
+
+
+inputBox.addEventListener('change', function(){
+    // if(e.key == "Enter"){
+        // logInput();
+        checkInputMatch();
         clearInputBox();
-    }
+    // }
 })
 
+buttonInit.addEventListener('click', function(){
+    genRandWord();
+    // console.log('clicked');
+})
