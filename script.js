2cad98d827b788a27a89edc9da0f1e8a2bc677c7
andicodetrf <andrea_lau92@hotmail.com>
2020-06-29 12:45:43 +0800

    created array to track top 3 score

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ let totalSecs = 5;
 let startInterval;
 let highScore = 0;
 let round = 0;
+let player = "";
+// let playerScore = totalScore;
+let topPlayersArray = [];
+let topScoreArray = [0,0,0];
+// let playersArray = [];
+let scoreArray = [0];
 
 //-------------------DOM SELECTORS----------------------------
 let wordDisplay = document.querySelector('.word-display');
@@ -23,15 +29,83 @@ let totalScoreDisplay = document.querySelector('.total-score');
 let buttonInit = document.querySelector('.btn-init');
 let nameDisplay = document.querySelector('.player-name');
 let highScoreDisplay = document.querySelector('.highest-score');
+let firstplace = document.querySelector('.first');
+let secondplace = document.querySelector('.second');
+let thirdplace = document.querySelector('.third');
 
 
 //-------------------FUNCTIONS----------------------------
+//GET PLAYER NAME 
+const getPlayerName = () => {
+    let getName = prompt(`hi what's your name?`)
+    nameDisplay.textContent = getName;
+    player = getName;
+}
 
-//GET PLAYER NAME OR DISPLAY GAME OVER
-// const getPlayerName = () => {
-//     let getName = prompt(`hi what's your name?`)
-//     nameDisplay.textContent = getName;
+
+const ranking = () => {
+    scoreArray.unshift(totalScore);
+    // playersArray.unshift(player);
+    if(totalScore > highScore) {
+        // topScoreArray.unshift(totalScore);
+        // topPlayersArray.unshift(player);
+        topScoreArray[0] = totalScore;
+
+    // } else if (scoreArray[0] <= topScoreArray[0] && scoreArray[0] > topScoreArray[1]) {
+
+    } else if (totalScore <= topScoreArray[0] && totalScore > topScoreArray[1]) {
+        topScoreArray[1] = totalScore;
+
+
+    // } else if (scoreArray[0] <= topScoreArray[1] && scoreArray[0] > topScoreArray[2]) {
+
+    } else if (totalScore <= topScoreArray[1] && totalScore > topScoreArray[2]) {
+        topScoreArray[2] = totalScore;
+
+    } 
+    console.log('topScArr --> ', topScoreArray);
+    console.log('collectionArr --> ', scoreArray);
+    console.log(totalScore);
+    // if(topScoreArray.length > 3){
+    //     topScoreArray.pop();
+    // }
+}
+
+
+
+// const updateFirstRank = () => {
+//         rankArray.unshift(player)
+//         scoreArray.unshift(totalScore)
+//         firstplace.textContent = `#1 ${rankArray[0]} ..... ${scoreArray[0]}`
 // }
+
+// const updateRestRank = () => {
+//     if(rankArray.length < 3){
+//         rankArray.splice(1, 0, player)
+//         scoreArray.splice(1, 0, totalScore)
+//         secondplace.textContent = `#2 ${rankArray[1]} ..... ${scoreArray[1]}`
+
+//     } else if (rankArray.length > 2 && rankArray < 4){
+//         if(totalScore < scoreArray[1]){
+//             rankArray.splice(2, 0, player)
+//             scoreArray.splice(2, 0, totalScore)
+//             thirdplace.textContent = `#3 ${rankArray[2]} ..... ${scoreArray[2]}`
+//         } else {
+//             rankArray.splice(1, 0, player)
+//             scoreArray.splice(1, 0, totalScore)
+//             secondplace.textContent = `#2 ${rankArray[1]} ..... ${scoreArray[1]}`
+//         }
+
+//     } else {
+//         if(totalScore > scoreArray[2]){
+//             rankArray.splice(2, 0, player)
+//             scoreArray.splice(2, 0, totalScore)
+//             thirdplace.textContent = `#3 ${rankArray[2]} ..... ${scoreArray[2]}`
+//         }
+//     }
+// }
+
+
 
 
 //BUTTON DISABLED WHEN GAME IN PROGRESS
@@ -47,7 +121,6 @@ const gameToStartBtn = () => {
     buttonInit.style.backgroundColor = "darkgreen";
     buttonInit.style.color = "white";
     buttonInit.disabled = false;
-
 }
 
 
@@ -108,8 +181,10 @@ const isHighScore = () =>{
     if(totalScore > highScore){
         highScore = totalScore;
         highScoreDisplay.textContent = highScore;
+        // updateFirstRank();
     } else {
         highScoreDisplay.textContent = highScore;
+        // updateRestRank();
     }
 }
 
@@ -128,7 +203,7 @@ const countdown = () =>{
         console.log('$$ Countdown END isGameStart', isGameStart)
 
         displayResult();
-        resultModal();
+        // resultModal();
         gameToStartBtn();
         wordDisplay.textContent = "-- word --";
         clearInputBox();
@@ -145,6 +220,7 @@ const countdown = () =>{
 const displayResult = () => {
     totalScore = currentScore
     totalScoreDisplay.textContent = totalScore;
+    ranking();
     isHighScore();
 }
 
